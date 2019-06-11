86c2132e858737b349d70ffa1ded795d3f306257
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-06-11 16:19:58 +0200

    Scala basics

--- My stuff/Scala basics/scala_programming.scala
+++ My stuff/Scala basics/scala_programming.scala
@@ -1,4 +1,6 @@
-val person = "Diego"
+import util.control.Breaks._
+
+/*val person = "Diego"
 
 if (person == "Sammy") {
   println("Welcome Sammy")
@@ -6,4 +8,22 @@ if (person == "Sammy") {
   println("Welcome George")
 } else {
   println(s"You're not welcome, $person")
+}*/
+
+/*for (num <- Range(0,11)) {
+  if (num % 2 == 0) {
+    println(s"$num is even")
+  } else {
+    println(s"$num is odd")
+  }
+}*/
+
+var x = 0
+
+while (x < 5) {
+  println(s"x is currenty $x")
+  println(s"$x is still less than 5, adding 1 to x")
+  if (x == 3)
+    break
+  x += 1
 }
