285beb9d1d0031b13950d4ad78caccbc162649c8
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-06-11 15:13:03 +0200

    Scala basics

---
+++ My stuff/scala_programming.scala
@@ -0,0 +1,9 @@
+val person = "Diego"
+
+if (person == "Sammy") {
+  println("Welcome Sammy")
+} else if (person == "George") {
+  println("Welcome George")
+} else {
+  println(s"You're not welcome, $person")
+}
